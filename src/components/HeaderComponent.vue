f442251b3c28a77719f221a97bb9ff802034bc6e
ValerioScharizza <valerio.schiarizza@live.it>
2022-09-23 17:25:16 +0200

    Dinamically generated imgs into main

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,127 +1,126 @@
 <template>
   <div class="container">
     <header>
-        <div id="logo">
-            <img src="../assets/img/dc-logo.png" alt="">
-        </div>
+      <div id="logo">
+        <img src="../assets/img/dc-logo.png" alt="" />
+      </div>
 
-        <nav>
-            <ul>
-                <li v-for="link in navLinks" :key="link.title">
-                    <a :href="link.link" :class="{ active: link.active }">{{ link.title }}</a>
-                </li>
-            </ul>
-        </nav>
-
+      <nav>
+        <ul>
+          <li v-for="link in navLinks" :key="link.title">
+            <a :href="link.link" :class="{ active: link.active }">{{
+              link.title
+            }}</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   </div>
 </template>
 
 <script>
 export default {
-name: 'HeaderComponent',
+  name: "HeaderComponent",
 
-data(){
+  data() {
     return {
-        navLinks: [
-            {
-                title: 'Characters',
-                link: '#',
-                active: false,
-            },
-            {
-                title: 'Comics',
-                link: '#',
-                active: true,
-            },
-            {
-                title: 'Movies',
-                link: '#',
-                active: false,
-            },
-            {
-                title: 'TV',
-                link: '#',
-                active: false,
-            },
-            {
-                title: 'Games',
-                link: '#',
-                active: false,
-            },
-            {
-                title: 'Collectibles',
-                link: '#',
-                active: false,
-            },
-            {
-                title: 'Videos',
-                link: '#',
-                active: false,
-            },
-            {
-                title: 'Fan',
-                link: '#',
-                active: false,
-            },
-            {
-                title: 'News',
-                link: '#',
-                active: false,
-            },
-            {
-                title: 'Shop',
-                link: '#',
-                active: false,
-            },
-        ]
-    }
-}
-}
+      navLinks: [
+        {
+          title: "Characters",
+          link: "#",
+          active: false,
+        },
+        {
+          title: "Comics",
+          link: "#",
+          active: true,
+        },
+        {
+          title: "Movies",
+          link: "#",
+          active: false,
+        },
+        {
+          title: "TV",
+          link: "#",
+          active: false,
+        },
+        {
+          title: "Games",
+          link: "#",
+          active: false,
+        },
+        {
+          title: "Collectibles",
+          link: "#",
+          active: false,
+        },
+        {
+          title: "Videos",
+          link: "#",
+          active: false,
+        },
+        {
+          title: "Fan",
+          link: "#",
+          active: false,
+        },
+        {
+          title: "News",
+          link: "#",
+          active: false,
+        },
+        {
+          title: "Shop",
+          link: "#",
+          active: false,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
-@import '../styles/variables.scss';
+@import "../styles/variables.scss";
 
 header {
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 
 #logo {
-    width: 80px;
-    margin: 20px 0px;
+  width: 80px;
+  margin: 20px 0px;
 
-    img {
-        vertical-align: middle;
+  img {
+    vertical-align: middle;
     height: 100%;
-    }
+  }
 }
 
 ul {
-    list-style-type: none;
-    height: 100%;
-    display: flex;
+  list-style-type: none;
+  height: 100%;
+  display: flex;
 
-    li {
+  li {
     height: 100%;
     padding: 0px 10px;
     font-weight: bold;
 
-        a {
-    text-decoration: none;
-    text-transform: uppercase;
-    color: #443c32;
-    display: flex;
-    align-items: center;
-    height: 100%;
-    &.active {
+    a {
+      text-decoration: none;
+      text-transform: uppercase;
+      color: #443c32;
+      display: flex;
+      align-items: center;
+      height: 100%;
+      &.active {
         border-bottom: 2px solid $dc-brand-color;
         color: #309efb;
+      }
     }
+  }
 }
-}
-}
-
 </style>
